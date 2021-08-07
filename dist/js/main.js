0593f6cd6f1458fb30fff926f49be50b0073aeec
Paul <hqdrone@gmail.com>
2021-08-08 02:25:16 +0300

    async changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,21 +4,44 @@ const App = {
 			search: '',
 			sort: 'desc',
 			users: [],
+			page: 1,
+			perPage: 12,
+			usersLength: null
 		}
 	},
 	methods: {
-		onSort() {
-			this.getUsers()
+		async onSort() {
+			this.page = 1
+			await this.getUsers()
 		},
-		onSearch: _.debounce(function () {
-			this.getUsers()
+		async onPrev() {
+			this.page -= 1
+			await this.getUsers()
+		},
+		async onNext() {
+			this.page += 1
+			await this.getUsers()
+		},
+		onSearch: _.debounce(async function () {
+			this.page = 1
+			await this.getUsers()
 		}, 2000),
 		async getUsers() {
 			if (this.search.length) {
-				const {data} = await axios.get(`https://api.github.com/search/users?q=${this.search}+in:login&sort=repositories&order=${this.sort}`)
-				this.users = data.items
+				try {
+					const {data} = await axios.get(`https://api.github.com/search/users?q=${this.search}+in:login&sort=repositories&order=${this.sort}&per_page=${this.perPage}&page=${this.page}`)
+					this.usersLength = data.total_count
+					this.users = data.items
+				} catch (e) {
+					console.log('too many requests - ' + e.message)
+				}
+				
 			}
-			
+		}
+	},
+	computed: {
+		isLastPage() {
+			return this.usersLength <= this.page * this.perPage
 		}
 	}
 }
