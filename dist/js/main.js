baef4320624e0a1a84999c6cc335e2a703d9b90b
Paul <hqdrone@gmail.com>
2021-08-08 16:43:10 +0300

    add router

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,83 @@
-const App = {
+const App = {}
+
+const app = Vue.createApp(App)
+
+app.component('app-nav', {
+	template: `
+		<div class="app__nav app-nav">
+			<ul>
+				<li>
+					<router-link to="/">Home</router-link>
+				</li>
+				<li>
+					<router-link to="/users">Users</router-link>
+				<li>
+			</ul>
+		</div>
+	`
+})
+
+const AppHome = {
+	template: `
+		<router-link to="/users">Users</router-link>
+	`
+}
+
+const AppUsers = {
+	template: `
+		<div class="app__tools app-tools">
+			<div class="app__search app-search">
+				<label>
+					Search
+					<input
+						type="text"
+						style="margin: 0 16px;"
+						v-model="search"
+						@input="onSearch">
+				</label>
+			</div>
+			<div class="app__sort app-sort">
+				<label>
+					Sort
+					<select
+						style="margin: 0 16px;"
+						v-model="sort"
+						@change="onSort">
+						<option value="desc">More repos</option>
+						<option value="asc">Less repos</option>
+					</select>
+				</label>
+			</div>
+		</div>
+
+		<div class="app__users app-users">
+			<div
+				class="app__user app-user"
+				v-for="user in users"
+				:key="user.id"
+			>
+				<div class="app-user__head">
+					<div class="app-user__avatar">
+						<img :src="user.avatar_url" alt="">
+					</div>
+					<div class="app-user__login">{{user.login}}</div>
+				</div>
+				<div class="app-user__body">
+					<div class="app-user__repos">
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div
+			class="app__pagination app-pagination"
+			v-if="this.users.length"
+		>
+			<button @click="onPrev" :disabled="isFirstPage">Prev</button>
+			<button @click="onNext" :disabled="isLastPage">Next</button>
+		</div>
+	`,
 	data() {
 		return {
 			search: '',
@@ -42,8 +121,22 @@ const App = {
 	computed: {
 		isLastPage() {
 			return this.usersLength <= this.page * this.perPage
+		},
+		isFirstPage() {
+			return this.page <= 1
 		}
 	}
 }
 
-Vue.createApp(App).mount('#app')
+const routes = [
+	{path: '/', component: AppHome},
+	{path: '/users', component: AppUsers}
+]
+
+const router = VueRouter.createRouter({
+	history: VueRouter.createWebHashHistory(),
+	routes
+})
+
+app.use(router)
+app.mount('#app')
